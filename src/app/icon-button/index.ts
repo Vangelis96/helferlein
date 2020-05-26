2057f3e4cd12d860b49f5ce1008ba8e9166a9859
Vangelis <vangelis.alsghir@students.fhnw.ch>
2020-05-26 09:39:56 +0200

    Page design

--- src/app/icon-button/index.ts
+++ src/app/icon-button/index.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { IconButtonComponent } from './icon-button.component';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
 	imports: [
 		MatIconModule,
-		CommonModule
+		CommonModule,
+		BrowserModule,
+		RouterModule,
 	],
 	exports: [
 		IconButtonComponent
